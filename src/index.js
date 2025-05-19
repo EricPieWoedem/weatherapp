bb79132997c3e150cdc0df16c6620e0b736a894c
EricPieWoedem <coco.pie3030@gmail.com>
2025-05-19 04:23:52 -1200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.css';
+
+const cityInput = document.getElementById('cityInput');
+const searchButton = document.getElementById('searchButton');
+const weatherInfo = document.getElementById('weatherInfo');
+
